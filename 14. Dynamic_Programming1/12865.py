34cfc8d2f39f1c6ba2cab82798cbea9ef4209b0d
ybkim95 <ybkim95@yonsei.ac.kr>
2021-02-24 05:54:00 +0900

    2021.02.23 TUE BAEK-JOON

--- 14. Dynamic_Programming1/12865.py
+++ 14. Dynamic_Programming1/12865.py
@@ -1,16 +1,35 @@
 import sys
 
-N, K = list(map(int, sys.stdin.readline().rsplit()))
+# 0-1 knapsack problem
 
-knapsack = [ list(map(int, sys.stdin.readline().rsplit())) for _ in range(N)]
-knapsack.sort()
+N,K = map(int, sys.stdin.readline().rsplit()) # Number of items, Weight Limit
 
-value = 0
-i = 0
-while K > 0:
-    value += knapsack[i][1]
-    K -= knapsack[i][0]
-    i += 1
+W=[0]
+V=[0]
 
-print(value)
+for _ in range(N):
+    w,v = map(int, sys.stdin.readline().rsplit()) # (weight,value) pair
+    W.append(w)
+    V.append(v)
 
+def Knapsack1(num_item,capacity): # 시간 초과
+    if capacity == 0 or num_item == 0:
+        return 0
+    elif W[num_item] > capacity:
+        return Knapsack1(num_item-1, capacity)
+    else:
+        return max( V[num_item-1] + Knapsack1(num_item-1, capacity-W[num_item]), Knapsack1(num_item-1, capacity) ) 
+
+def Knapsack2(n,c):
+    dp = [[0]*(c+1) for _ in range(n+1)]
+
+    for i in range(1,n+1):
+        for w in range(c,0,-1):
+            if w >= W[i]:
+                dp[i][w] = max( V[i] + dp[i-1][w-W[i]], dp[i-1][w] )
+            else:
+                dp[i][w] = dp[i-1][w]
+    
+    return dp[n][c]
+
+print(Knapsack2(N,K))
